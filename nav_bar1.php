df60af4555f2e821e6a2ecb0945b9d7082288b62
chibuzo5 <chieezy5@gmail.com>
2019-10-03 07:07:43 +0100

    this is a php file that holds the nav bar for pages like index.php etc

---
+++ nav_bar1.php
@@ -0,0 +1,34 @@
+<?php
+
+echo  ('
+<nav class="overlay navbar navbar-expand-lg navbar-trans fixed-top " id="mainNav">
+                        <div class="container">
+                                <a class="js-scroll-trigger navbar-brand nav-item active" href="#body"><i class="fas fa-coins "></i> Coin</a>
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                  <span class="navbar-toggler-icon"></span>
+                                </button>
+                              
+                     <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                     <div class="navbar-nav align-items-end">
+                       <ul class="navbar-nav mr-auto">
+                                                    
+                             <li class="nav-item">
+                              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                             </li>
+                             
+                               <li class="nav-item">
+                             <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                               <!-- Button trigger modal -->
+                              <a class="nav-link " href="" >  Sign up</a>
+                             </li>
+                             <li class="nav-item">
+                              <a class="nav-link " href=""  data-toggle="modal" data-target="#ModalCenter"> Login</a>
+                             </li>
+                      </ul>
+                     </div>
+                         </div>
+                        </div>
+                     </nav>');
+                     
