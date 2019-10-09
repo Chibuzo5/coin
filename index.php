3e56c19ceef623c09c1280dfe8fe04f5efe87726
chibuzo5 <chieezy5@gmail.com>
2019-10-09 11:50:23 +0100

    changed google font

--- index.php
+++ index.php
@@ -5,23 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Coin</title>
     <meta name="description" content="">
+    
    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <link href="https://fonts.googleapis.com/css?family=Hepta+Slab:400,700&display=swap" rel="stylesheet">
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Lora&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="./css/main.css">
   </head>
-  <body>
+  <body class="home_background">
 
    <div>
     <?php
     include 'nav_bar1.php';
     ?>
    </div>
-   
+              
+              <div class="masthead mx-auto">
+                    <div class="container d-flex h-100 align-items-center">
+                      <div class="mx-auto text-center index_text">
+                        <h1 class="mx-auto my-30 text-uppercase display-3 text-light-50">Welcome to coin</h1>
+                        <h2 class=" mx-auto mt-2 mb-5 text-light-50">Onestop for all financial transations</h2>
+                      
+                      </div>
+                    </div>
+                </div>
     
   </body>
 </html>
