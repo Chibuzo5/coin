ef2efb55acacd28ea92b0f9f1ac73562f35c44e4
chibuzo5 <chieezy5@gmail.com>
2019-11-05 22:18:06 +0100

    intial commit

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Coin</title>
+    <meta name="description" content="">
+    
+   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Lora&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/main.css">
+  </head>
+<body class="about_page" >
+   <div>
+    <?php
+    include 'nav_bar1.php';
+    ?>
+    <style>
+      
+      </style>
+   </div>
+      <div class="body" style="color:white; text-align:center;">
+        <div class="container">
+          <h2 class="text-uppercase display-3   " >About Coin </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi iure ducimus ad quas doloremque accusamus
+           perferendis pariatur laboriosam optio, nisi porro debitis cumque assumenda repellendus hic sint blanditiis 
+           praesentium maiores aut. Quae animi velit, mollitia quibusdam porro molestiae dolor, temporibus maxime 
+           voluptas sunt natus. Magnam, nostrum minus. Quos fugiat error, ex aspernatur aut maxime dolorum est possimus 
+           iusto, corrupti officia. Ex ullam, provident veniam, magni, ratione esse magnam odit corporis neque sed doloremque 
+           culpa commodi vitae ab? Optio consequuntur corrupti ducimus reiciendis maxime odio nam est illo necessitatibus, 
+           impedit aspernatur et modi exercitationem obcaecati perferendis nostrum deserunt, libero sed vero eius veniam nobis 
+           repellat praesentium error! Vel blanditiis alias officia dolorum quidem, itaque, sint corrupti eius tenetur ducimus 
+           nemo voluptatibus, inventore doloribus. Officiis, quas vel? Aspernatur quidem libero facilis veniam cum in repellat 
+           quae officiis, pariatur magnam,
+           animi delectus doloribus? Et deserunt totam nostrum, nam dolor quisquam modi dolorum veniam?</p>
+
+           
+           <a href="https://twitter.com/nathanbolt5?ref_src=twsrc%5Etfw" class="twitter-follow-button" 
+           data-size="large" data-show-count="false">Follow @nathanbolt5</a>
+           <script defer async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
+        <div>
+</body>
+</html>
+ 
